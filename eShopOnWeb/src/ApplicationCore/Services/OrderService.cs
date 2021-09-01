81db4f9563e272af8b91e5ff9ebcefba9e48d51c
Bekarys Kuralbay <Bekarys.Kuralbay@thomsonreuters.com>
2021-09-01 18:06:21 +0600

    add eshop

---
+++ eShopOnWeb/src/ApplicationCore/Services/OrderService.cs
@@ -0,0 +1,88 @@
+﻿using Ardalis.GuardClauses;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IAsyncRepository<Order> _orderRepository;
+        private readonly IUriComposer _uriComposer;
+        private readonly IAsyncRepository<Basket> _basketRepository;
+        private readonly IAsyncRepository<CatalogItem> _itemRepository;
+
+        public OrderService(IAsyncRepository<Basket> basketRepository,
+            IAsyncRepository<CatalogItem> itemRepository,
+            IAsyncRepository<Order> orderRepository,
+            IUriComposer uriComposer)
+        {
+            _orderRepository = orderRepository;
+            _uriComposer = uriComposer;
+            _basketRepository = basketRepository;
+            _itemRepository = itemRepository;
+        }
+
+        public async Task CreateOrderAsync(int basketId, Address shippingAddress)
+        {
+            var basketSpec = new BasketWithItemsSpecification(basketId);
+            var basket = await _basketRepository.FirstOrDefaultAsync(basketSpec);
+
+            Guard.Against.NullBasket(basketId, basket);
+            Guard.Against.EmptyBasketOnCheckout(basket.Items);
+
+            var catalogItemsSpecification = new CatalogItemsSpecification(basket.Items.Select(item => item.CatalogItemId).ToArray());
+            var catalogItems = await _itemRepository.ListAsync(catalogItemsSpecification);
+
+            var items = basket.Items.Select(basketItem =>
+            {
+                var catalogItem = catalogItems.First(c => c.Id == basketItem.CatalogItemId);
+                var itemOrdered = new CatalogItemOrdered(catalogItem.Id, catalogItem.Name, _uriComposer.ComposePicUri(catalogItem.PictureUri));
+                var orderItem = new OrderItem(itemOrdered, basketItem.UnitPrice, basketItem.Quantity);
+                return orderItem;
+            }).ToList();
+
+            var order = new Order(basket.BuyerId, shippingAddress, items);
+            var json = JsonConvert.SerializeObject(order);
+
+            await SendToFunction(json);
+            SendToFunctionApp(json);
+            await _orderRepository.AddAsync(order);
+        }
+
+        private async Task SendToFunction(string json)
+        {
+            var queueClient = new QueueClient("Endpoint=sb://bekasb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=vRotYpxxRMXDNmK98/Nh1NmrrLWBv9tsKZCJulcfWfo=", "requests");
+
+            var encodedMessage = new Message(Encoding.UTF8.GetBytes(json));
+            await queueClient.SendAsync(encodedMessage);
+        }
+
+        private void SendToFunctionApp(string json)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:7071/api/Function1");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+        }
+    }
+}
